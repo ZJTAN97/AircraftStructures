36e6efabcf4dc8adf932f82d4c451414f393dd69
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-10-13 22:06:27 +0800

    Update BendingMoment.py

--- BendingMoment.py
+++ BendingMoment.py
@@ -2,9 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 bending_moments = [117720, 529740, 735750, 662175, 294300]
+shear_forces = [186390, 147150, 88290, 39240, 9810]
 
 stacked_moments = []
 current_bending = sum(bending_moments)
+distance = [0, 3, 9, 15, 22.5, 30.0]
+
 
 for moment in bending_moments:
     current_bending = current_bending - moment
@@ -13,12 +16,23 @@ for moment in bending_moments:
 stacked_moments.insert(0, sum(bending_moments))
 
 
+distance_2 = [3, 6, 6, 7.5, 7.5]
+area = []
+
+for i, force in enumerate(shear_forces):
+    area.append(force * distance_2[i])
 
 
-distance = [0, 3, 9, 15, 22.5, 30.0]
+stacked_area = []
+current_area = sum(area)
+for item in area:
+    current_area = current_area - item
+    stacked_area.append(current_area)
+
+stacked_area.insert(0, sum(area))
 
 
-plt.plot(distance, [moment*-1 for moment in stacked_moments], marker='o', color='b')
+plt.plot(distance, [area*-1 for area in stacked_area], marker='o', color='b')
 plt.xlabel('Distance (m)')
 plt.ylabel('Moments (Nm) ')
 plt.ylim([-sum(bending_moments), sum(bending_moments)])
